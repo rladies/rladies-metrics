f82fbf741839ea338710929162b9e6eb25d7bfb3
Gabriela de Queiroz <gabidequeiroz@gmail.com>
2018-05-03 14:10:24 -0700

    Add shiny app for metrics

---
+++ app.R
@@ -0,0 +1,106 @@
+
+source("data.R")
+
+
+
+# Header ----------------------------------------------------------------------
+
+header <- dashboardHeader(title = "R-Ladies Metrics Dashboard",
+                          titleWidth = 200)
+
+# Sidebar ---------------------------------------------------------------------
+# sidebar <- dashboardSidebar(
+#   selectInput(
+#     inputId = "rladies",
+#     label = "City:", 
+#     choices = rladies_list, 
+#     selectize = FALSE)
+# )
+
+sidebar <- dashboardSidebar(disable = TRUE)
+
+# Body --------------------------------------------------------------------- 
+body <- dashboardBody(
+  
+  tabsetPanel(
+    id = "tabs",
+    
+    tabPanel(
+      title = "Main Dashboard",
+      value = "page1",
+      
+      fluidRow(
+        
+        absolutePanel( # class = "panel panel-default panel-side",
+          style = "z-index: 2000", fixed = TRUE, draggable = TRUE,
+          top  = 10, left = "auto", right = 20, width = "250px",
+          div(
+            tags$a(target="_blank", 
+                   href = "http://www.rladies.org", 
+                   tags$img(src="R-LadiesGlobal_RBG_online_LogoWithText.png", 
+                            height = "30px", id = "logo") 
+            )
+          )
+        ),
+        
+        # Info boxes
+        valueBox(n_countries, "Countries", icon("globe", "font-awesome"), width = 2),
+        valueBox(n_cities, "Cities", icon("globe", "font-awesome"), width = 2),
+        valueBox(n_has_meetup_page, "Has meetup page", icon("meetup", lib = "font-awesome"), 
+                 width = 2, color = "red"),
+        valueBoxOutput("n_tweets", width = 2)
+        
+      ),
+      
+      fluidRow(
+        
+        # Tables
+        box(
+          title = "Need to be added to the repo",
+          width = 3,
+          tableOutput("tbl_meetup_not_on_gh"),
+          status = "warning",
+          collapsible = TRUE
+        )
+      )
+      
+    )
+  )
+  
+  
+)
+
+
+
+ui <- dashboardPage(skin = "purple", header, sidebar, body)
+
+
+
+
+
+server <- function(input, output) { 
+  
+  
+  output$n_tweets <- renderValueBox({
+    valueBox(value = n_rladies_chapters_twitter, "R-Ladies on Twitter", icon = icon("twitter"), color = "purple" )
+  })
+  
+  
+  meetup_not_on_gh <- as.data.frame(meetup_not_on_gh)
+  colnames(meetup_not_on_gh) <- "meetup name"
+  output$tbl_meetup_not_on_gh <- renderTable(meetup_not_on_gh, rownames = FALSE)
+    
+        
+
+  
+}
+  
+  
+  
+  
+  
+
+
+
+
+shinyApp(ui, server)
