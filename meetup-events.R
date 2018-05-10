3e0be3ca1f269b2b62715e861bbe60380c494026
Gabriela de Queiroz <gabidequeiroz@gmail.com>
2018-05-09 21:28:46 -0700

    Add comments + api key

--- meetup-events.R
+++ meetup-events.R
@@ -1,4 +1,10 @@
 library(meetupr)
+library(lubridate)
+
+# Need to setup the MEETUP KEY and read it
+api_key <- readRDS("meetup_key.RDS")
+
+
 # slowly function from Jenny Bryan
 slowly <- function(f, delay = 0.5) {
   function(...) {
@@ -12,20 +18,24 @@ slowly <- function(f, delay = 0.5) {
 
 # rladies_groups
 # need to wrap in safely - meetups that have not met (yet) throw an error, which seems to be causing map() to fail
-rl_meetups_past <- map(rladies_groups$urlname, slowly(safely(get_events)), event_status = c("past"))
+# this takes a few seconds to download
+rl_meetups_past <- map(rladies_groups$urlname, slowly(safely(get_events)), 
+                       event_status = c("past"), api_key = api_key)
 
-str(rl_meetups_past, max.level = 1)
+# str(rl_meetups_past, max.level = 1)
 
 
-x <- lapply(seq_along(rl_meetups_past), 
-       function(x) rl_meetups_past[[x]]$result[,c("local_date", "venue_city", "link")])
+subset_past_meetups <- lapply(seq_along(rl_meetups_past), 
+                              function(x) rl_meetups_past[[x]]$result[,c("local_date", "venue_city", "link")])
 
-past_meetups <- bind_rows(x)
+past_meetups <- bind_rows(subset_past_meetups)
+
+# Clean up url to get the city name
 past_meetups$meetup_url <- gsub("events.*","", past_meetups$link)
 past_meetups$city <- gsub("-|/|_", "", 
      gsub(pattern = ".*(rladies|r-ladies|R-Ladies|RLadies)", "", past_meetups$meetup_url)
 )
-unique(past_meetups$city)
+# unique(past_meetups$city)
 
 total_number_events <- past_meetups %>%
   filter(!is.na(venue_city)) %>% 
