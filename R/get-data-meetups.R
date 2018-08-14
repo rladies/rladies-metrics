a009c99c45e88f1dbb912941760bf0db2822b272
Gabriela de Queiroz <gabidequeiroz@gmail.com>
2018-08-13 17:08:14 -0700

    Add log info :cop:

--- R/get-data-meetups.R
+++ R/get-data-meetups.R
@@ -38,9 +38,10 @@ slowly <- function(f, delay = 0.3) {
 # this takes a few seconds to download
 rl_meetups_past <- map(rladies_groups$urlname, slowly(safely(get_events)), 
                        event_status = c("past"), api_key = api_key)
-
+futile.logger::flog.info("-------- Downloading meetup info ----------")
 # str(rl_meetups_past, max.level = 1)
 
+futile.logger::flog.info("Length meetup: %s", length(rl_meetups_past))
 
 subset_past_meetups <- lapply(seq_along(rl_meetups_past), 
                               function(x) rl_meetups_past[[x]]$result[,c("local_date", "venue_city", "link")])
@@ -54,6 +55,7 @@ past_meetups$city <- gsub("-|/|_", "",
 )
 # unique(past_meetups$city)
 
+futile.logger::flog.info("Dataset rows: %s", dim(past_meetups))
 
 # -------------------------------------------------------------------
 # Save the Data on dropbox 
